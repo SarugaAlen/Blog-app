5b0235926d7e222e50fda7d4da12209b54b5c13e
Alen <alen.201dva@gmail.com>
2023-08-01 10:41:35 +0200

    project setup commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const port = 3000;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.listen(port, () => {
+    console.log('Server deluje na portu: ${port}');
+})
