4bae1e303db268ddcaf9dd030caaaadbe7346d04
Alen <alen.201dva@gmail.com>
2023-08-02 14:23:14 +0200

    testing dev  branch

--- frontend/my-app/src/routes/+page.svelte
+++ frontend/my-app/src/routes/+page.svelte
@@ -3,5 +3,7 @@
 	$: currentHour = currentDate.getHours();
 </script>
 
-<div class="w-screen h-screen gradient bg-gradient-to-r from-blue-400 via-purple-600 to-pink-600 animate-gradient"></div>
+<div class="flex flex-col items-center w-screen h-screen gradient bg-gradient-to-r from-blue-400 via-purple-600 to-pink-600 animate-gradient">
+	<h1 class="text-white font-bold mt-14 text-7xl font-mono">Dobrodošli</h1>
+</div>
 
